fad84efbc49c3fddc472487669dcf84f816f303c
user <email@email.com>
2019-02-05 01:35:18 +0400

    Start working with rest-api: added list and detail views for Article model

---
+++ articles/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from Nau4OK.settings import AUTH_USER_MODEL
+from articles.models import Article, User
+
+admin.site.register(Article)
+admin.site.register(User, UserAdmin)
---
+++ articles/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from Nau4OK.settings import AUTH_USER_MODEL
+
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Article(models.Model):
+    author = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
+    text = models.TextField()
+    image = models.ImageField(upload_to='article_images/')
+    rating = models.IntegerField(default=0)
+    n_comments = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
