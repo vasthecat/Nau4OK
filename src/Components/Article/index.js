f215710738cfb0f077086d0aceb79bf998bc930d
Andrew <saintruler@gmail.com>
2019-02-04 15:52:38 +0400

    Updated Article's look, added LESS

--- src/Components/Article/index.js
+++ src/Components/Article/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import './style.css'
+import './styles.less'
 
 
 class Article extends Component {
@@ -8,49 +8,19 @@ class Article extends Component {
     };
 
     render() {
-        const {article, isBigSized} = this.props;
-
-        const colClass = "col-lg-" + (isBigSized ? 8 : 4) + " col-md-" + (isBigSized ? 12 : 6);
+        const {article} = this.props;
+        const image = require('../../static/image.jpg');
 
         return (
-            <div className={colClass}>
-                <div className="card" style={{height: '400px'}}>
-                    <div className="card-body card_bg">
-                        <div className="container">
-
-                            <div className="row">
-                                <h2 className="col-12 text-center card-title">
-                                    {article.title}
-                                </h2>
-                            </div>
-
-                            <div className="row">
-                                <div className="col-2"></div>
-
-                                {/*Horizontal line*/}
-                                <div
-                                    className="col-8"
-                                    style={{'height': '2px', 'background-color': 'rgb(255, 236, 132)'}}
-                                ></div>
-
-                                <div className="col-2"></div>
-                            </div>
-
-                            <div className="row">
-                                <p className="col-12 card-text my-4">
-                                    {article.body.split(" ").slice(0, (isBigSized ? 100 : 50)).join(" ")}
-                                </p>
-                            </div>
-
-                            <div className="row">
-                                <h4 className="col-12 card-text">Creation date: {article.date}</h4>
-                            </div>
-
-                        </div>
-                    </div>
+            <div className="card">
+                <div className="card__image" style={{'background-image': 'url(' + image +')'}}></div>
+                <div className="card__content">
+                    <div className="card__title">{article.title}</div>
+                    <p className="card__text">{article.body.split(" ").slice(0, 50).join(" ")}</p>
+                    <button className="btn btn--block card__btn">Button</button>
                 </div>
             </div>
-        )
+        );
     }
 
     handleClick = () => {
--- src/Components/ArticleList.js
+++ src/Components/ArticleList.js
@@ -1,36 +1,23 @@
 import React from 'react'
 import Article from './Article'
+import './Article/styles.css'
 
 export default function ArticleList({ articles }) {
-    // var elements = [];
-    // var temp = [];
-    // var j = 0;
-    // articles.forEach(function (item, i, arr) {
-    //     i++;
-    //     var isBigSized = (i - 4) % 7 === 0 || i % 7 === 0;
-    //     if (isBigSized) {
-    //         j += 2
-    //     } else {
-    //         j++
-    //     }
-    //     temp.push(<Article article={item} isBigSized={isBigSized}/>);
-    //     // elements[elements.length - 1].push(<Article article={item} isBigSized={isBigSized}/>);
-    //     if (j % 3 === 0) {
-    //         elements.push(<div className="row mb-4">{temp}</div>);
-    //         temp = [];
-    //     }
-    //     j %= 3;
-    // });
     var i = 0;
     const articleElements = articles.map(article => {
         i++;
-        return (<Article article={article} isBigSized={(i - 4) % 7 === 0 || i % 7 === 0}/>)
+        const isBigSized = (i - 4) % 7 === 0 || i % 7 === 0;
+        const liClass = (isBigSized ? "cards__item--big" : "cards__item");
+        return (
+            <li className={liClass}>
+                <Article article={article}/>
+            </li>
+        )
     });
 
     return (
-        <div className="row no-gutters">{articleElements}</div>
+        <ul className="cards">
+            {articleElements}
+        </ul>
     )
-    // return (
-    //     <div>{elements}</div>
-    // )
 }
